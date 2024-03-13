a9e83213f8deaf4302527690c8e7659f267cb987
pernik36 <pernik36@centrum.cz>
2024-03-13 11:19:20 +0100

    added config, world generation, simulation launch/stop

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,3 +1,6 @@
+import os
+import signal
+import subprocess
 import sys
 from typing import Optional
 from PySide6 import QtCore as qtc
@@ -5,9 +8,13 @@ from PySide6 import QtWidgets as qtw
 from PySide6 import QtGui as qtg
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QWidget
+import psutil
 
 from missions import Ui_MainWindow
 import yaml
+
+import xml.etree.ElementTree as ET
+import math
 
 # class RotatableContainer(qtw.QGraphicsView):
 #     def __init__(self, widget: QWidget, rotation: float):
@@ -45,6 +52,7 @@ class PositionUpdater():
 class Missions(qtw.QMainWindow):
     def __init__(self, parent=None):
         super(Missions, self).__init__(parent=parent)
+        self.load_config()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
@@ -53,7 +61,12 @@ class Missions(qtw.QMainWindow):
         self.setup_signals_slots()
         self.setup_states()
 
-
+    def load_config(self):
+        try:
+            with open("config.yaml", "r") as file:
+                self.cfg = yaml.load(file, Loader=yaml.SafeLoader)
+        except FileNotFoundError:
+            print("Config not found.")      
 
     def setup_signals_slots(self):
         def sss_mise():
@@ -63,6 +76,7 @@ class Missions(qtw.QMainWindow):
             self.ui.list_mise.clicked.connect(self.mission_selected)
             self.ui.list_mise.activated.connect(self.mission_selected)
             self.ui.list_mise.doubleClicked.connect(self.load_mission)
+            self.ui.but_start.clicked.connect(self.run_stop_mission)
 
         sss_mise()
         pass
@@ -72,11 +86,12 @@ class Missions(qtw.QMainWindow):
         self.ui.tabWidget.setTabEnabled(2, False)   # tab not ready -> disable
         self.mise = MissionsModel()                            # dict of missions, mission name as key
         self.ui.list_mise.setModel(self.mise)
+        self.px4 = None
 
     def save_mission(self):
         nazev = self.ui.textBox_nazev_mise.toPlainText()
         if not nazev: return
-        self.mise.save(self.tabMise_mission())
+        self.mise.save(self.tabMise_mission_dict())
 
     def delete_mission(self):
         nazev = self.ui.textBox_nazev_mise.toPlainText()
@@ -126,7 +141,7 @@ class Missions(qtw.QMainWindow):
         except ValueError:
             pass
 
-    def tabMise_mission(self):
+    def tabMise_mission_dict(self):
         pl_x = self.ui.pl_x.value()
         pl_y = self.ui.pl_y.value()
         pl_phi = self.ui.pl_phi.value()
@@ -158,6 +173,89 @@ class Missions(qtw.QMainWindow):
     
     def mission_selected(self, index):
         self.ui.textBox_nazev_mise.setPlainText(self.mise.names[index.row()])
+
+    def stop_mission(self):
+        self.px4.send_signal(signal.SIGINT)
+        self.px4.communicate()
+        for proc in psutil.process_iter():
+            if "ruby" in proc.name():
+                proc.send_signal(signal.SIGINT)
+        self.px4 = None
+
+    def run_mission(self):
+        if not self.ui.textBox_nazev_mise.toPlainText():
+            self.ui.textBox_nazev_mise.setPlainText("BezNazvu")
+
+        command = [self.cfg["px4_bin_path"]]
+        env_vars = {
+            'PX4_SYS_AUTOSTART': self.cfg["px4_sys_autostart"],
+            'PX4_GZ_MODEL': self.cfg["gz_model"],
+            'PX4_GZ_WORLD': self.ui.textBox_nazev_mise.toPlainText(),
+            'PX4_GZ_MODEL_POSE': " ".join((str(self.ui.uav_x.value()), str(self.ui.uav_y.value()), "0", "0", "0", str(self.ui.uav_phi.value())))
+        }
+
+        self.update_sdf_with_mission(self.ui.textBox_nazev_mise.toPlainText()+".sdf")
+
+        original_env = os.environ.copy()
+
+        # Update environment with the new variables
+        env = original_env.copy()
+        env.update(env_vars)
+
+        self.px4 = subprocess.Popen(command, env=env, stdin=subprocess.PIPE)
+
+        os.environ.clear()
+        os.environ.update(original_env)
+
+    def run_stop_mission(self):
+        if self.px4 is None:
+            self.run_mission()
+            self.ui.but_start.setText("■ Stop")
+            return
+        self.stop_mission()
+        self.ui.but_start.setText("▶ Start")
+
+    def update_sdf_with_mission(self, output_file):
+        tree = ET.parse(self.cfg["base_world_path"])
+        root = tree.getroot()
+
+        mission = self.tabMise_mission_dict()
+
+        include = root.find(".//include")           # umisteni tagu
+        pose_element = include.find("pose")
+        pose_x = mission['plosina']['x']
+        pose_y = mission['plosina']['y']
+        pose_yaw = mission['plosina']['phi']
+        pose_values = f"{pose_x} {pose_y} 0 0 1.57 {pose_yaw}"
+        pose_element.text = pose_values
+
+        wind = root.find(".//wind")                 # vitr
+        linear_velocity_element = wind.find("linear_velocity")
+        wind_phi = mission['vitr']['phi']
+        wind_v = mission['vitr']['v']
+        wind_speed_x = wind_v * math.cos(wind_phi)
+        wind_speed_y = wind_v * math.sin(wind_phi)
+        linear_velocity_values = f"{wind_speed_x} {wind_speed_y} 0"
+        linear_velocity_element.text = linear_velocity_values
+
+        plugin = root.find(".//plugin[@name='gz::sim::systems::WindEffects']")              # sum vetru
+        horizontal = plugin.find("./horizontal")
+        direction = horizontal.find("./direction")
+        noise_s_phi = mission['vitr']['s_phi']
+        noise_s_v = mission['vitr']['s_v']
+        direction_noise = direction.find("./noise")
+        direction_noise_stddev = direction_noise.find("./stddev")
+        direction_noise_stddev.text = str(noise_s_phi)
+
+        magnitude = horizontal.find("./magnitude")
+        magnitude_noise = magnitude.find("./noise")
+        magnitude_noise_stddev = magnitude_noise.find("./stddev")
+        magnitude_noise_stddev.text = str(noise_s_v)
+
+        world = root.find(".//world")
+        world.set("name", mission["nazev"])
+
+        tree.write(os.path.join(self.cfg["gz_worlds_dir"], output_file))
     
 class MissionsModel(qtc.QAbstractListModel):
     def __init__(self, missionList=None):
